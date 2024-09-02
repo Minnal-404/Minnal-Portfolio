d71162953e28753dda91f6626082bda8044127e0
Minnal-404 <jeshindanielofficial@gmail.com>
2024-09-02 14:30:25 +0530

    Initial Commit-Portfolio Website

---
+++ index.js
@@ -0,0 +1,89 @@
+// let principal = 10000;
+// let rate = 5;
+// let time = 3;
+// let si = (principal*rate*time)/100;
+// console.log("Simple Interest = $" + si);
+
+// console.log("=================================================");
+
+// let radius = 14;
+// const pi = 3.14159;
+// let area = pi*radius**2;
+// console.log("Area = " + area);
+
+// console.log("=================================================");
+
+// let priceA = 150;
+// let priceB = 200;
+// priceA = priceA + priceB;
+// priceB = priceA - priceB;
+// priceA = priceA - priceB;
+// console.log("Price A = " + priceA);
+// console.log("Price B = " + priceB);
+
+// console.log("=================================================");
+
+// let num1 = 85;
+// let num2 = 90;
+// let num3 = 95;
+// let average = (num1 + num2 + num3)/3;
+// console.log("Average = " + average);
+
+// console.log("=================================================");
+
+// let amount = 5000;
+// let irate = 6;
+// let years = 4;
+// let ci = amount*(1+(irate/100))^years;
+// console.log("Compound Interest = $" + ci);
+
+// console.log("=================================================");
+
+// let cp = 800;
+// let sp = 950;
+// let profit = sp-cp;
+// console.log("Profit = " + profit);
+
+// console.log("=================================================");
+
+// let l = 50;
+// let w = 30;
+// let perimeter = 2*(l+w);
+// console.log("Perimeter = " + perimeter);
+
+// console.log("=================================================");
+
+// let x = 25;
+// let y = 19;
+// let and = x&y;
+// console.log("AND = " + and);
+
+// console.log("=================================================");
+
+// let celsius = 22;
+// let fahrenheit = (celsius*9/5) + 32;
+// console.log("Fahrenheit = " + fahrenheit);
+
+// console.log("=================================================");
+
+// let r = 5;
+// let h = 10;
+// let volume = pi*r**2*h;
+// console.log("Volume = " + volume);
+// let num1 = -12;
+// // let num2 = -12;
+// // let mathMark = 83;
+// if (num1>0){
+//     console.log("The number is positive");
+// }else if(num1==0){
+//     console.log("The number is zero")
+// }else{
+//     console.log("The number is negative");
+// }
+let letter = 'o';
+
+if (letter=='a'||letter=='e'||letter=='i'||letter=='o'||letter=='u'){
+    console.log("The character is vowel");
+}else{
+    console.log("The character is consonant");
+}
